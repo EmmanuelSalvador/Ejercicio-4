ea3ee5798573c72eae1cc2705d1c4f221cf734d6
EmmanuelSalvador <emsacervantesdi@ittepic.edu.mx>
2019-10-29 10:39:45 -0700

    Validar nombre

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,6 +15,7 @@ export class HomePage implements OnInit {
     this.myForm = this.fb.group( {
       // tslint:disable-next-line: max-line-length
       RFC: ['', Validators.compose([Validators.required, Validators.pattern('[A-Z,Ñ,&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z,0-9]?[A-Z,0-9]?[0-9,A-Z]?')])],
+      name: ['', Validators.required],
       email: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])],
       // tslint:disable-next-line: max-line-length
       curp: ['', Validators.compose([Validators.required, Validators.pattern('[A-Z,Ñ,&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z,0-9]?[A-Z,0-9]?[0-9,A-Z]?')])]
