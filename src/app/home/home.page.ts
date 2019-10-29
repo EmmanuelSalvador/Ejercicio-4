c6ff12efb96d80978bef969e0d5408de5f241bc4
EmmanuelSalvador <emsacervantesdi@ittepic.edu.mx>
2019-10-29 09:39:05 -0700

    Ejercicio 4

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, FormArray, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
-
+  myForm: FormGroup;
+  constructor(private router:Router, private fb: FormBuilder) {}
+  ngOnInit() {
+    this.myForm = this.fb.group( {
+      // tslint:disable-next-line: max-line-length
+      RFC: ['', Validators.compose([Validators.required, Validators.pattern('[A-Z,Ñ,&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z,0-9]?[A-Z,0-9]?[0-9,A-Z]?')])],
+      email: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])],
+      // tslint:disable-next-line: max-line-length
+      curp: ['', Validators.compose([Validators.required, Validators.pattern('[A-Z,Ñ,&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z,0-9]?[A-Z,0-9]?[0-9,A-Z]?')])]
+    });
+  }
 }
